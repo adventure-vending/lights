155ccb4cc7ea77113b5b3b4eb01c64dd0400102e
Travis Gary <travis.gary@airbnb.com>
2016-08-02 21:12:26 -0700

    three working classes for selectBox, openBox and Idle

--- lights.py
+++ lights.py
@@ -14,62 +14,63 @@ pds = PowerSupply(ip_address)
 
 # address light fixtures using lowest dmx address
 # example if using RGB addresses 3,4,5 choose 5
-fix00 = FixtureRGB(12)
-fix01 = FixtureRGB(24)
-fix02 = FixtureRGB(0)
-fix03 = FixtureRGB(0)
-fix04 = FixtureRGB(0)
-fix05 = FixtureRGB(0)
-fix06 = FixtureRGB(0)
-fix07 = FixtureRGB(0)
-fix08 = FixtureRGB(0)
-fix09 = FixtureRGB(0)
-fix10 = FixtureRGB(0)
-fix11 = FixtureRGB(0)
-fix12 = FixtureRGB(0)
-fix13 = FixtureRGB(0)
-fix14 = FixtureRGB(0)
-fix15 = FixtureRGB(0)
-fix16 = FixtureRGB(0)
-fix17 = FixtureRGB(0)
-fix18 = FixtureRGB(0)
-fix19 = FixtureRGB(0)
-fix20 = FixtureRGB(0)
-fix21 = FixtureRGB(0)
-fix22 = FixtureRGB(0)
-fix23 = FixtureRGB(0)
+fix00 = FixtureRGB(0)
+fix01 = FixtureRGB(6)
+#fix02 = FixtureRGB(0)
+#fix03 = FixtureRGB(0)
+#fix04 = FixtureRGB(0)
+#fix05 = FixtureRGB(0)
+#fix06 = FixtureRGB(0)
+#fix07 = FixtureRGB(0)
+#fix08 = FixtureRGB(0)
+#fix09 = FixtureRGB(0)
+#fix10 = FixtureRGB(0)
+#fix11 = FixtureRGB(0)
+#fix12 = FixtureRGB(0)
+#fix13 = FixtureRGB(0)
+#fix14 = FixtureRGB(0)
+#fix15 = FixtureRGB(0)
+#fix16 = FixtureRGB(0)
+#fix17 = FixtureRGB(0)
+#fix18 = FixtureRGB(0)
+#fix19 = FixtureRGB(0)
+#fix20 = FixtureRGB(0)
+#fix21 = FixtureRGB(0)
+#fix22 = FixtureRGB(0)
+#fix23 = FixtureRGB(0)
 
 # Attach each of the 24 light fixtures to the power supply
 # TODO, automate appending based on the number of fixtures defined.
 pds.append(fix00)
 pds.append(fix01)
-pds.append(fix02)
-pds.append(fix03)
-pds.append(fix04)
-pds.append(fix05)
-pds.append(fix06)
-pds.append(fix07)
-pds.append(fix08)
-pds.append(fix09)
-pds.append(fix10)
-pds.append(fix11)
-pds.append(fix12)
-pds.append(fix13)
-pds.append(fix14)
-pds.append(fix15)
-pds.append(fix16)
-pds.append(fix17)
-pds.append(fix18)
-pds.append(fix19)
-pds.append(fix20)
-pds.append(fix21)
-pds.append(fix22)
-pds.append(fix23)
+#pds.append(fix02)
+#pds.append(fix03)
+#pds.append(fix04)
+#pds.append(fix05)
+#pds.append(fix06)
+#pds.append(fix07)
+#pds.append(fix08)
+#pds.append(fix09)
+#pds.append(fix10)
+#pds.append(fix11)
+#pds.append(fix12)
+#pds.append(fix13)
+#pds.append(fix14)
+#pds.append(fix15)
+#pds.append(fix16)
+#pds.append(fix17)
+#pds.append(fix18)
+#pds.append(fix19)
+#pds.append(fix20)
+#pds.append(fix21)
+#pds.append(fix22)
+#pds.append(fix23)
 
 #Remove this, just for testing lights
-#pds[0].rgb = (0, 0, 0)
-#pds[1].rgb = (255, 255, 255)
+#pds[1].rgb = (0, 0, 0)
+#pds[0].rgb = (255, 255, 255)
 #pds.go()
+#time.sleep(0.1)
 
 # example of how to use the FadeIter
 def fader(pds1, cnt):
@@ -103,7 +104,7 @@ def selectBox(pds, box, pause=.1, steps=1000):
               ratio += (step + idx * div) % steps / float(steps)
               fixture.hsv = (ratio, 1.0, 1.0)
         print pds
-        #pds.go()
+        pds.go()
         time.sleep(pause)
 
 def openBox(pds, box):
@@ -112,6 +113,17 @@ def openBox(pds, box):
   #not sure if this looks good or not
       selectBox(pds, box, steps=10)
 
+def idle(pds, pause=.1, steps=1000):
+    div = steps / len(pds)
+    for step in range(steps):
+        ratio = 0
+        for idx, fixture in enumerate(pds):
+            ratio += (step + idx * div) % steps / float(steps)
+            fixture.hsv = (ratio, 1.0, 1.0)
+        print pds
+        pds.go()
+        time.sleep(pause)
+
 
 #TODO, write a idle routine that is more aware of the placement of lights and does a cool animation
 
@@ -119,6 +131,6 @@ def openBox(pds, box):
 #TODO, add a test here to change the prize box every few seconds to simulate user input
 #TODO, use the fader class to transition between user inputs
 #selectBox(pds, 6)
-openBox(pds, 6)
+openBox(pds, 1)
 
 
